55324dd9cdc04d319c36f684a4b284babe18d06f
Veronika Pristromskaya <76497091+MaliyOna@users.noreply.github.com>
2023-11-06 10:59:13 +0100

    set up configuration database (#4)

---
+++ CatNote.DAL/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CatNote.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace CatNote.DAL;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<AchievementEntity> Achievements { get; set; }
+    public DbSet<TaskEntity> Tasks { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
