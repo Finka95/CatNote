fc4ef01c64d1e814e5e9ac016ca8c43d749733f1
Veronika Pristromskaya <76497091+MaliyOna@users.noreply.github.com>
2023-11-06 15:31:58 +0100

    create extensions methods for DI in BLL and DAL (#6)
    * create extensions methods for DI in BLL and DAL
    
    * change property to field

---
+++ CatNote.BLL/DI/DependencyRegistration.cs
@@ -0,0 +1,20 @@
+﻿using CatNote.DAL.Entities;
+using CatNote.DAL.Repositories.Interfaces;
+using CatNote.DAL.Repositories;
+using CatNote.DAL;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatNote.DAL.DI;
+
+namespace CatNote.BLL.DI;
+public static class DependencyRegistration
+{
+    public static void AddBusinessServices(this IServiceCollection services, string connectionString)
+    {
+        services.AddDatabaseServices(connectionString);
+    }
+}
