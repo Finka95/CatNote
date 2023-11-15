267e52d934fdda174e95f6d404753b2edc221d63
Vlad Marshin <infin95@gmail.com>
2023-11-15 12:49:49 +0100

    Update IGenericService.cs

--- CatNote.BLL/Interfaces/IGenericService.cs
+++ CatNote.BLL/Interfaces/IGenericService.cs
@@ -6,5 +6,6 @@ public interface IGenericService<TModel>
     Task Delete(int id, CancellationToken cancellationToken);
     Task<IEnumerable<TModel>> GetAll(CancellationToken cancellationToken);
     Task<TModel> Update(TModel element, CancellationToken cancellationToken);
+    
     Task<TModel> GetById(int id, CancellationToken cancellationToken);
 }
