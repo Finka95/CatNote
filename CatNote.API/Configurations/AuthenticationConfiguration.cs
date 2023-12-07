854644de71554f06b28397c0a25e024f434b7259
MaliyOna <veronika.pr777@gmail.com>
2023-12-07 21:52:13 +0300

    add auth0

---
+++ CatNote.API/Configurations/AuthenticationConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CatNote.API.Configurations;
+
+public class AuthenticationConfiguration
+{
+    public string ClientSecret { get; set; }
+
+    public SecurityKey GetSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.ASCII.GetBytes(ClientSecret));
+}
--- CatNote.API/Program.cs
+++ CatNote.API/Program.cs
@@ -1,8 +1,13 @@
-﻿using CatNote.API.Mappers;
+﻿using System.Security.Claims;
+using CatNote.API.Configurations;
+using CatNote.API.Mappers;
 using CatNote.API.Middlewares;
 using CatNote.BLL.DI;
 using CatNote.BLL.Mappers;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace CatNote.API;
 
@@ -22,14 +27,88 @@ public class Program
 
         builder.Services.AddBusinessServices(connection);
 
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+
+        var authConfig = builder.Configuration.GetSection("Auth0").Get<AuthenticationConfiguration>();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+            options.Audience = builder.Configuration["Auth0:Audience"];
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                NameClaimType = ClaimTypes.NameIdentifier,
+
+                ValidateIssuerSigningKey = false,
+                IssuerSigningKey = authConfig.GetSecurityKey()
+            };
+        });
+
+        builder.Services.AddAuthentication();
+
         builder.Services.AddAutoMapper(typeof(MapperApiProfile).Assembly, typeof(MapperBllProfile).Assembly);
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "API Documentation",
+                Version = "v1.0",
+                Description = ""
+            });
+            options.ResolveConflictingActions(x => x.First());
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                BearerFormat = "JWT",
+                Flows = new OpenApiOAuthFlows
+                {
+                    AuthorizationCode = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri($"https://{builder.Configuration["Auth0:Domain"]}/authorize?audience={builder.Configuration["Auth0:Audience"]}"),
+                        TokenUrl = new Uri($"https://{builder.Configuration["Auth0:Domain"]}/oauth/token"),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { "openid", "OpenId" },
+
+                        }
+                    }
+                }
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                    },
+                    new[] { "openid" }
+                }
+            });
+        });
 
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1.0");
+                c.OAuthClientId(builder.Configuration["Auth0:ClientId"]);
+                c.OAuthClientSecret(builder.Configuration["Auth0:ClientSecret"]);
+                c.OAuthUsePkce();
+            });
         }
 
         app.UseMiddleware<ExceptionHandlingMiddleware>();
