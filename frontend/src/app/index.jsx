95f3ef4da976fe06d1e5daeea2661c6eef1e0d98
Veronika Pristromskaya <veronika.pr777@gmail.com>
2023-12-07 16:20:00 +0100

    delete

--- frontend/src/app/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useUser } from "@auth0/nextjs-auth0/dist/client";
-
-export default function index() {
-    const { user, error, isLoading } = useUser();
-
-    return <a href="/api/auth/login">Login</a>
-}
