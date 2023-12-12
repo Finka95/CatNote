9e37f9b8132c87990fd096381df0e6d3267b99fb
Veronika Pristromskaya <veronika.pr777@gmail.com>
2023-12-12 08:47:52 +0100

    move api dependencies registration

--- CatNote.API/Configurations/AuthenticationConfiguration.cs
+++ CatNote.API/Configurations/AuthenticationConfiguration.cs
@@ -5,6 +5,10 @@ namespace CatNote.API.Configurations;
 
 public class AuthenticationConfiguration
 {
+    public string Domain { get; set; } = null!;
+
+    public string Audience { get; set; } = null!;
+
     public string ClientSecret { get; set; } = null!;
 
     public SecurityKey GetSecurityKey() =>
--- CatNote.API/Program.cs
+++ CatNote.API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Claims;
 using CatNote.API.Configurations;
+using CatNote.API.DI;
 using CatNote.API.Mappers;
 using CatNote.API.Middlewares;
 using CatNote.BLL.DI;
@@ -13,8 +14,6 @@ namespace CatNote.API;
 
 public class Program
 {
-    private static string[] openIdArray = { "openid" };
-
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
@@ -29,73 +28,9 @@ public class Program
 
         builder.Services.AddBusinessServices(connection);
 
-        builder.Services.AddCors(options =>
-        {
-            options.AddDefaultPolicy(
-                builder =>
-                {
-                    builder.WithOrigins("http://localhost:3000")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
-        });
-
         var authConfig = builder.Configuration.GetSection("Auth0").Get<AuthenticationConfiguration>();
 
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
-                options.Audience = builder.Configuration["Auth0:Audience"];
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    NameClaimType = ClaimTypes.NameIdentifier,
-
-                    ValidateIssuerSigningKey = false,
-                    IssuerSigningKey = authConfig!.GetSecurityKey()
-                };
-            });
-
-        builder.Services.AddAutoMapper(typeof(MapperApiProfile).Assembly, typeof(MapperBllProfile).Assembly);
-
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Title = "API Documentation",
-                Version = "v1.0",
-                Description = ""
-            });
-            options.ResolveConflictingActions(x => x.First());
-            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-            {
-                Type = SecuritySchemeType.OAuth2,
-                BearerFormat = "JWT",
-                Flows = new OpenApiOAuthFlows
-                {
-                    AuthorizationCode = new OpenApiOAuthFlow
-                    {
-                        AuthorizationUrl = new Uri($"https://{builder.Configuration["Auth0:Domain"]}/authorize?audience={builder.Configuration["Auth0:Audience"]}"),
-                        TokenUrl = new Uri($"https://{builder.Configuration["Auth0:Domain"]}/oauth/token"),
-                        Scopes = new Dictionary<string, string>
-                        {
-                            { "openid", "OpenId" },
-
-                        }
-                    }
-                }
-            });
-            options.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
-                    },
-                    openIdArray
-                }
-            });
-        });
+        builder.Services.AddApiServices(authConfig!);
 
         var app = builder.Build();
 
