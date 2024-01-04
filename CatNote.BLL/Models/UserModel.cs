988c12342b13b3723665840c461173b8dfe081ac
Veronika Pristromskaya <veronika.pr777@gmail.com>
2024-01-04 10:28:48 +0100

    add userRole to user

--- CatNote.BLL/Models/UserModel.cs
+++ CatNote.BLL/Models/UserModel.cs
@@ -4,6 +4,7 @@ public class UserModel
 {
     public int Id { get; set; }
     public string? UserName { get; set; }
+    public bool IsAdmin { get; set; }
     public IEnumerable<TaskModel>? Tasks { get; set; }
     public IEnumerable<AchievementModel>? Achievements { get; set;}
 }
--- CatNote.DAL/Entities/UserEntity.cs
+++ CatNote.DAL/Entities/UserEntity.cs
@@ -3,6 +3,7 @@
 public class UserEntity : BaseEntity
 {
     public string? UserName { get; set; } = null!;
+    public bool IsAdmin { get; set; }
     public ICollection<TaskEntity>? Tasks { get; set; }
     public ICollection<AchievementEntity>? Achievements { get; set; }
 }
