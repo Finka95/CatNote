49cab2941603666d70dd60a677354e411b698c41
Veronika Pristromskaya <76497091+MaliyOna@users.noreply.github.com>
2024-01-05 11:35:36 +0300

    add userRole to user (#26)

--- CatNote.API/DTO/UserDTO.cs
+++ CatNote.API/DTO/UserDTO.cs
@@ -4,6 +4,7 @@ public class UserDTO
 {
     public int Id { get; set; }
     public string? UserName { get; set; }
+    public bool IsAdmin { get; set; }
     public IEnumerable<TaskDTO>? Tasks { get; set; }
     public IEnumerable<AchievementDTO>? Achievements { get; set; }
 }
